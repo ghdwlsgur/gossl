7bae11869cfc2c4b22086db5745803d791476467
HongJinHyeok <vjinhyeokv@gmail.com>
2022-11-11 15:52:40 +0900

    사용자로부터 파일 선택

--- internal/client.go
+++ internal/client.go
@@ -6,7 +6,27 @@ type (
 	Domain struct {
 		Name string
 	}
+
+	ReqDomain struct {
+		Name string
+	}
 )
+
+func AskCertFile(FileList []string) (string, error) {
+	prompt := &survey.Select{
+		Message: "choose file",
+		Options: FileList,
+	}
+
+	answer := ""
+	if err := survey.AskOne(prompt, &answer, survey.WithIcons(func(icons *survey.IconSet) {
+		icons.SelectFocus.Format = "green+hb"
+	}), survey.WithPageSize(20)); err != nil {
+		return "", err
+	}
+
+	return answer, nil
+}
 
 func AskDomain() (*Domain, error) {
 	var domainName string
@@ -22,4 +42,21 @@ func AskDomain() (*Domain, error) {
 	}
 
 	return &Domain{Name: domainName}, nil
+}
+
+func AskReqDomain() (*ReqDomain, error) {
+	var reqDomainName string
+
+	prompt := &survey.Input{
+		Message: "What is your request domain ?",
+	}
+
+	if err := survey.AskOne(prompt, &reqDomainName, survey.WithIcons(func(icons *survey.IconSet) {
+		icons.SelectFocus.Format = "green+hb"
+	}), survey.WithPageSize(2)); err != nil {
+		return nil, err
+	}
+
+	return &ReqDomain{Name: reqDomainName}, nil
+
 }
