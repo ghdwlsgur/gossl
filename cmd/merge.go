23071fd55c9fe56c7938c0dc6b014aa2ffd0b5c4
HongJinHyeok <vjinhyeokv@gmail.com>
2022-11-18 01:06:53 +0900

    update ephemeral

--- cmd/merge.go
+++ cmd/merge.go
@@ -2,8 +2,8 @@ package cmd
 
 import (
 	"encoding/pem"
+	"fmt"
 	"os"
-	"strings"
 
 	"github.com/ghdwlsgur/cert-check/internal"
 	"github.com/spf13/cobra"
@@ -16,6 +16,12 @@ var (
 		Long:  "",
 		Run: func(_ *cobra.Command, _ []string) {
 
+			arg, err := internal.GetArguments(os.Args)
+			if err != nil {
+				panicRed(err)
+			}
+			newFile := fmt.Sprintf("%s.pem", arg)
+
 			certFile, err = internal.Dir()
 			if err != nil {
 				panicRed(err)
@@ -26,45 +32,117 @@ var (
 				panicRed(err)
 			}
 
-			t, err := os.Create("test.pem")
+			n := len(selectList)
+			if n > 3 {
+				panicRed(fmt.Errorf("please select up to 3"))
+			}
+
+			// if n < 2 {
+			// 	panicRed(fmt.Errorf("please select at least 2"))
+			// }
+
+			// if n == 2 {
+			// 	n += 1
+			// }
+
+			t, err := os.Create(newFile)
 			if err != nil {
 				panicRed(err)
 			}
 			defer t.Close()
 
-			var k = [3]*pem.Block{}
+			// k := make([]*pem.Block, n)
 
 			for _, s := range selectList {
-
-				l := strings.Split(s, ".")
-				certFile.Extension = strings.Split(s, ".")[len(l)-1]
+				internal.SetCertExtension(certFile, s)
 
 				p, err = internal.GetPemType(s)
+				// if err != nil {
+				// 	panicRed(err)
+				// }
+
+				data, err := os.ReadFile(s)
 				if err != nil {
 					panicRed(err)
 				}
-
-				if p.Type == "CERTIFICATE" {
-
-					detail, err := internal.DistinguishCertificate(p, certFile)
-					if err != nil {
+				var blocks []byte
+				p = &internal.Pem{}
+				for {
+					var block *pem.Block
+					block, data = pem.Decode(data)
+					if block == nil {
+						break
+					}
+					if err := pem.Encode(t, block); err != nil {
 						panicRed(err)
 					}
-
-					if detail == "Leaf Certificate" {
-						k[0] = p.Block
-					} else if detail == "Intermediate Certificate" {
-						k[1] = p.Block
-					} else if detail == "Root Certificate" {
-						k[2] = p.Block
+					blocks = append(blocks, block.Bytes...)
+					if len(data) == 0 {
+						break
 					}
 				}
-			}
 
-			for _, v := range k {
-				if err := pem.Encode(t, v); err != nil {
-					panicRed(err)
-				}
+				// c, err := x509.ParseCertificates(blocks)
+				// if err != nil {
+				// 	panicRed(err)
+				// }
+				// for _, asd := range c {
+				// 	fmt.Println(asd.PublicKey.(*rsa.PublicKey).N)
+				// }
+
+				// p, _ := pem.Decode(data)
+
+				// if err := pem.Encode(os.Stdout, p.Block); err != nil {
+				// 	panicRed(err)
+				// }
+
+				// 	if p.Type == "CERTIFICATE" {
+				// 		detail, err := internal.DistinguishCertificate(p, certFile)
+				// 		if err != nil {
+				// 			panicRed(err)
+				// 		}
+
+				// 		if detail == "Leaf Certificate" {
+				// 			k[0] = p.Block
+				// 		} else if detail == "Intermediate Certificate" {
+				// 			k[1] = p.Block
+				// 			// fmt.Println(base64.StdEncoding.EncodeToString(p.Block.Bytes))
+				// 		} else if detail == "Root Certificate" {
+				// 			k[2] = p.Block
+				// 		}
+				// 	} else {
+				// 		panicRed(fmt.Errorf("please select only the certificate file"))
+				// 	}
+				// }
+
+				// var blocks []byte
+				// for _, v := range k {
+				// 	// fmt.Println("g")
+				// 	// rest := v.Bytes
+				// 	// for {
+				// 	// 	var block *pem.Block
+				// 	// 	block, rest := pem.Decode(rest)
+				// 	// 	if block == nil {
+				// 	// 		fmt.Println("go")
+				// 	// 		break
+				// 	// 	}
+				// 	// 	blocks = append(blocks, block.Bytes...)
+				// 	// 	if len(rest) == 0 {
+				// 	// 		break
+				// 	// 	}
+				// 	// 	if err := pem.Encode(os.Stdout, block); err != nil {
+				// 	// 		panicRed(err)
+				// 	// 	}
+				// 	// }
+
+				// 	if err := pem.Encode(t, v); err != nil {
+				// 		panicRed(err)
+				// 	}
+
+				// 	// if err := pem.Encode(os.Stdout, b); err != nil {
+				// 	// 	panicRed(err)
+				// 	// }
+
 			}
 
 		},
